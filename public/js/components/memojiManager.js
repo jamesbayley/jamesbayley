e26e7dd12c280416162c5706332e874708aa12b4
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-03-05 22:34:00 +0000

    Media Queries handled by CSS.

--- public/js/components/memojiManager.js
+++ public/js/components/memojiManager.js
@@ -8,7 +8,7 @@ class MemojiManager extends HTMLElement {
        
     const template = document.createElement('template');
     template.innerHTML = `
-      <link rel="stylesheet" href="../../styles.css" />
+      <link rel="stylesheet" href="../../css/styles.css" />
 
       <heart-memoji></heart-memoji>
       <phone-call-memoji></phone-call-memoji>
@@ -17,29 +17,6 @@ class MemojiManager extends HTMLElement {
 
     this.attachShadow({ mode: 'open' });
     this.shadowRoot?.appendChild(template.content.cloneNode(true));
-
-    this._memojis = { 
-      heart: this.shadowRoot.querySelector('heart-memoji'),
-      phoneCall: this.shadowRoot.querySelector('phone-call-memoji'),
-      waving: this.shadowRoot.querySelector('waving-memoji')
-    };
-
-    Object.values(this._memojis).forEach(memoji => { 
-      memoji.style.visibility = 'hidden';
-      console.log(memoji);
-    });
-  }
-
-  connectedCallback() { 
-    this._memojis.waving.style.visibility = 'visible';
-
-    if (window.matchMedia('(min-width: 600px)').matches) { 
-      this._memojis.phoneCall.style.visibility = 'visible';
-    }
-
-    if (window.matchMedia('(min-width: 1200px)').matches) { 
-      this._memojis.heart.style.visibility = 'visible';
-    }
   }
 }
 
